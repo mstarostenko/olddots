422779daafe9e32fcd92ea4eca8e655c59515ce1
Maxim Starostenko <m.starostenko@office.ngs.ru>
2015-11-22 14:16:19 +0300

    0: init commit

---
+++ fish/functions/fish_mode_prompt.fish
@@ -0,0 +1,23 @@
+# The fish_mode_prompt function is prepended to the prompt
+function fish_mode_prompt --description "Displays the current mode"
+  # Do nothing if not in vi mode
+  if set -q __fish_vi_mode
+    switch $fish_bind_mode
+      case default
+        set_color --bold --background red white
+	echo ':_ '
+      case insert
+        set_color --bold green
+	echo '⌨  '
+        # echo ' ✍ '
+      case replace-one
+        set_color --bold green
+        echo ' ꔸ '
+      case visual
+        set_color --bold --background magenta white
+        echo '[V]'
+    end
+    set_color normal
+    echo -n ' '
+  end
+end
